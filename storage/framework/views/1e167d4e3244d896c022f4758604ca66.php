f6a8ddfd69789350ec474820b5b19543846f684b
menezesmateus <mateusgithub12@gmail.com>
2024-06-28 20:18:02 -0300

    feat: add composer install

---
+++ storage/framework/views/1e167d4e3244d896c022f4758604ca66.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../app.css">
+    <title>Art Luminiere</title>
+</head>
+<body>
+<header class="frame-1">
+  <a href="/" class="art-lumi-re">ART LUMIÈRE</a>
+  <div class="frame-19">
+    <div class="frame-20">
+      <div class="bem-vindo-admin">Bem vindo, Admin</div>
+      <a href="/user"><img class="icon-person" src="../icon_person.svg" /></a>
+    </div>
+  </div>
+</header>
+<div id="container">
+    <?php echo $__env->yieldContent('content'); ?>
+
+</div>
+<footer class="frame-2">
+  <div class="criado-com-3-pelo-pitucao">Criado com &lt;3 pelo Pitucao</div>
+</footer>
+
+
+</body>
+</html><?php /**PATH C:\Users\Mateus Price\Documents\ArtLuminiere\resources\views/layouts/index.blade.php ENDPATH**/ ?>
